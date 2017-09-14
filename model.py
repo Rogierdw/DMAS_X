cf8d65b5fa38b02be3fcb75cb48dc371a7ce90e7
Rogier de Weert <rogier92@gmail.com>
2017-09-14 14:37:45 +0200

    no money given now, because Singlegrid can only have one agent per cell

--- model.py
+++ model.py
@@ -11,14 +11,38 @@ class FootballAgent(Agent):
         self.aggression = 1
 
     def move(self):
+        "Function for moving to empty cell next to agent"
+
+        moved = False
+        tries = 0
+        tried = []
+
         possible_steps = self.model.grid.get_neighborhood(
             self.pos,
             moore=True,             # Moore includes diagonal neighbors. Von Neumann only up/down/left/right
             include_center=False)   # Center cell does or does not count as neighbor
-        new_position = random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
+
+        while not moved and tries <= 8:
+            new_position = random.choice(possible_steps)
+            if new_position not in tried:
+                tried += new_position
+                if self.model.grid.is_cell_empty(new_position):
+                    moved = True
+                    self.model.grid.move_agent(self, new_position)
+                else:
+                    tries += 1
+
+        #new_position = random.choice(possible_steps)
+        #self.model.grid.move_agent(self, new_position)
 
     def give_aggression(self):
+
+        neighborhood = self.model.grid.get_neighborhood(
+            self.pos,
+            moore=True,  # Moore includes diagonal neighbors. Von Neumann only up/down/left/right
+            include_center=False)  # Center cell does or does not count as neighbor
+
+        #cellmates = [agent for agent in self.model.grid.get_cell_list_contents([neighborhood])]
         cellmates = self.model.grid.get_cell_list_contents([self.pos])
         if len(cellmates) > 1:
             other = random.choice(cellmates)
@@ -42,7 +66,7 @@ class AggressionModel(Model):
     def __init__(self, N, width, height):
         self.running = True # enables conditional shut off of the model (is now set True indefinitely)
         self.num_agents = N
-        self.grid = MultiGrid(width, height, True) # Boolean is for wrap-around, SingleGrid would enforce one agent/cell
+        self.grid = SingleGrid(width, height, True) # Boolean is for wrap-around, SingleGrid would enforce one agent/cell
         self.schedule = RandomActivation(self) # Means agent activation ordering is random
 
         # Create agents
@@ -52,11 +76,11 @@ class AggressionModel(Model):
 
 
             # Add the agent to a random grid cell
-            x = random.randrange(self.grid.width)
-            y = random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
+            #x = random.randrange(self.grid.width)
+            #y = random.randrange(self.grid.height)
+            #self.grid.place_agent(a, (x, y))
 
-            '''
+
             x = 1
             y = 1
             placed = False
@@ -67,7 +91,7 @@ class AggressionModel(Model):
                 else:
                     x = random.randrange(self.grid.width)
                     y = random.randrange(self.grid.height)
-            '''
+
 
         self.datacollector = DataCollector(
             model_reporters={"Gini": compute_gini},
