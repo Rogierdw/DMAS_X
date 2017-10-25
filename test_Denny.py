4d675bbfc1fe0c67cd55120330b84a9713ff757b
Rogier de Weert <rogier92@gmail.com>
2017-10-25 13:56:20 +0200

    changed to actual one-party

--- test_Denny.py
+++ test_Denny.py
@@ -1,54 +1,54 @@
 from model import *
 
-model = AggressionModel(N_fan = 338, N_hool = 112, N_pol = 30, N_riopol = 20, twogroup_switch = True)
+model = AggressionModel(N_fan = 338, N_hool = 112, N_pol = 30, N_riopol = 20, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('338-112-30-20-F.csv')
 
-model = AggressionModel(N_fan = 338, N_hool = 112, N_pol = 25, N_riopol = 25, twogroup_switch = True)
+model = AggressionModel(N_fan = 338, N_hool = 112, N_pol = 25, N_riopol = 25, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('338-112-25-25-F.csv')
 
-model = AggressionModel(N_fan = 338, N_hool = 112, N_pol = 40, N_riopol = 10, twogroup_switch = True)
+model = AggressionModel(N_fan = 338, N_hool = 112, N_pol = 40, N_riopol = 10, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('338-112-40-10-F.csv')
 
-model = AggressionModel(N_fan = 225, N_hool = 225, N_pol = 30, N_riopol = 20, twogroup_switch = True)
+model = AggressionModel(N_fan = 225, N_hool = 225, N_pol = 30, N_riopol = 20, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('225-225-30-20-F.csv')
 
-model = AggressionModel(N_fan = 225, N_hool = 225, N_pol = 25, N_riopol = 25, twogroup_switch = True)
+model = AggressionModel(N_fan = 225, N_hool = 225, N_pol = 25, N_riopol = 25, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('225-225-25-25-F.csv')
 
-model = AggressionModel(N_fan = 225, N_hool = 225, N_pol = 40, N_riopol = 10, twogroup_switch = True)
+model = AggressionModel(N_fan = 225, N_hool = 225, N_pol = 40, N_riopol = 10, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('200-200-40-10-F.csv')
 
-model = AggressionModel(N_fan = 394, N_hool = 56, N_pol = 30, N_riopol = 20, twogroup_switch = True)
+model = AggressionModel(N_fan = 394, N_hool = 56, N_pol = 30, N_riopol = 20, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('394-56-30-20-F.csv')
 
-model = AggressionModel(N_fan = 394, N_hool = 56, N_pol = 25, N_riopol = 25, twogroup_switch = True)
+model = AggressionModel(N_fan = 394, N_hool = 56, N_pol = 25, N_riopol = 25, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('394-56-25-25-F.csv')
 
-model = AggressionModel(N_fan = 394, N_hool = 56, N_pol = 40, N_riopol = 10, twogroup_switch = True)
+model = AggressionModel(N_fan = 394, N_hool = 56, N_pol = 40, N_riopol = 10, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
--- test_Leon.py
+++ test_Leon.py
@@ -1,54 +1,54 @@
 from model import *
 
-model = AggressionModel(N_fan = 188, N_hool = 62, N_pol = 150, N_riopol = 100, twogroup_switch = True)
+model = AggressionModel(N_fan = 188, N_hool = 62, N_pol = 150, N_riopol = 100, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('188-62-150-100-F.csv')
 
-model = AggressionModel(N_fan = 188, N_hool = 62, N_pol = 125, N_riopol = 125, twogroup_switch = True)
+model = AggressionModel(N_fan = 188, N_hool = 62, N_pol = 125, N_riopol = 125, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('188-62-125-125-F.csv')
 
-model = AggressionModel(N_fan = 188, N_hool = 62, N_pol = 200, N_riopol = 50, twogroup_switch = True)
+model = AggressionModel(N_fan = 188, N_hool = 62, N_pol = 200, N_riopol = 50, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('188-62-200-50-F.csv')
 
-model = AggressionModel(N_fan = 125, N_hool = 125, N_pol = 150, N_riopol = 100, twogroup_switch = True)
+model = AggressionModel(N_fan = 125, N_hool = 125, N_pol = 150, N_riopol = 100, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('125-125-150-100-F.csv')
 
-model = AggressionModel(N_fan = 125, N_hool = 125, N_pol = 125, N_riopol = 125, twogroup_switch = True)
+model = AggressionModel(N_fan = 125, N_hool = 125, N_pol = 125, N_riopol = 125, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('225-225-125-125-F.csv')
 
-model = AggressionModel(N_fan = 125, N_hool = 125, N_pol = 200, N_riopol = 50, twogroup_switch = True)
+model = AggressionModel(N_fan = 125, N_hool = 125, N_pol = 200, N_riopol = 50, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('125-125-200-50-F.csv')
 
-model = AggressionModel(N_fan = 219, N_hool = 31, N_pol = 150, N_riopol = 100, twogroup_switch = True)
+model = AggressionModel(N_fan = 219, N_hool = 31, N_pol = 150, N_riopol = 100, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('219-31-150-100-F.csv')
 
-model = AggressionModel(N_fan = 219, N_hool = 31, N_pol = 125, N_riopol = 125, twogroup_switch = True)
+model = AggressionModel(N_fan = 219, N_hool = 31, N_pol = 125, N_riopol = 125, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
 data.to_csv('219-31-125-125-F.csv')
 
-model = AggressionModel(N_fan = 219, N_hool = 31, N_pol = 200, N_riopol = 50, twogroup_switch = True)
+model = AggressionModel(N_fan = 219, N_hool = 31, N_pol = 200, N_riopol = 50, twogroup_switch = False)
 for i in range(5400):
     model.step()
 data = model.datacollector.get_model_vars_dataframe()
